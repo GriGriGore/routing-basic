b8ed8e7d757e267f5fa68f59d04c50dbf0ac4a50
Ovidiu Turcoane <ovidiuturcoane@yahoo.com>
2019-09-24 21:52:12 +0300

    'Init.'

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// Readers: To prepare this project to build along in `./App.js`, complete
+// steps 1 & 2 below
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App.js";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import "./index.css";
+import "./App.css";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import "./semantic-dist/semantic.min.css";
+
+// [STEP 1] Comment out this line:
+import "./index-complete";
+
+// [STEP 2] Un-comment this line:
+// ReactDOM.render(<App />, document.getElementById("root"));
